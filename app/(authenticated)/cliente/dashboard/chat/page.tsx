19b3e7c313edffc7af6f0e29192e61c8f2c05192
Gustavo Henrique <109995734+GustavoHenriqueProjects@users.noreply.github.com>
2023-11-21 09:02:22 -0300

    Atualizado

--- app/(authenticated)/cliente/dashboard/chat/page.tsx
+++ app/(authenticated)/cliente/dashboard/chat/page.tsx
@@ -1,28 +1,35 @@
 "use client"
-import getConsole from "./calc"
 
+import { io } from "socket.io-client"
+
+//Comando para instalar o socket client: npm install socket.io-client
 
 export default function Chat(){
 
-    const a: any = ["aaaa", "bbb", "ccc", "ddd", "eee", "fff"]
-    const b = "aaaaaa"
+    const socket = io('http://localhost:8181')
 
     let token = localStorage.getItem("token")
 
+    socket.on('connect', function(){
+        console.log('Conectado ao servidor Socket.io com o ID:', socket.id);
+
+        const room = {
+                serviceMysqlId: 1
+        }
+
+        // Vincula a mensagem apenas para a sala correspondente ao serviço
+        socket.emit('class', room)
+
+    })
+
     return(
         <>
-       <h1>teste</h1>
-       <span>{token}</span>
-        {
-            
-            a.map((abc: any) => {
-                return(
-                    <div>
-                    </div>
-                )
-            })
-        }
-       </>
+       <form id="chat">
+        <div className="messages"></div>
+        <input id="message" placeholder=" Digite sua mensagem"/>
+        <button type="submit">Enviar</button>
+    </form>
+        </>
     )
 }
 
